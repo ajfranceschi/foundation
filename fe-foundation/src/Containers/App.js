b92bfc1013c6422e54e286914332334bccc58c20
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-08-19 14:58:22 -0400

    organized file structure

---
+++ fe-foundation/src/Containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App container">
+      <div className='jumbotron'>
+        <h1>Jumbotron</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
