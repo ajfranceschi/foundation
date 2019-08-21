626239b2264b6e64898e4b6ddc02964ae606bc8a
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-08-21 17:11:51 -0400

    be: connected knex to heroku db

--- be-foundation/server.js
+++ be-foundation/server.js
@@ -3,13 +3,32 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const knex = require('knex');
 
+
+// declare db and configure knex's connection.
 const db = knex({
     client : 'pg',
     connection : {
-        host: '127.0.0.1',
-        user: '',
-        password: '',
-        database: 'foundation_db'
+        host: 'ec2-23-23-182-18.compute-1.amazonaws.com',
+        user: 'awweymdmpvbxwp',
+        password: '0f8a10fc068a58a568a7ccf7c5b5e51152140d62d5ef283f80b616d1f94ac11a',
+        database: 'd4oososc631cjs',
+        ssl: true
     }
 });
 
+const app = express();
+
+app.use('/', (req, res) => {
+   res.send('<h1> Hello Node.js </h1>');
+
+   // db('users').returning('id').insert({
+   //     firstname: 'FirstName',
+   //     lastname: 'LastName',
+   //     email: 'email3@domain.com'
+   // })
+});
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000.');
+});
