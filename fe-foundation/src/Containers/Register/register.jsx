0cb309ea6bb2b9e8579961cc6e7feec289d5ae59
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-08-25 22:43:57 -0400

    fe-foundation: completed registration dummy UI

--- fe-foundation/src/Containers/Register/register.jsx
+++ fe-foundation/src/Containers/Register/register.jsx
@@ -8,6 +8,47 @@ class Register extends Component {
             <div className="App">
                 <Navbar onRouteChange={this.props.onRouteChange}/>
                 <h1 className='mt-5'>Register</h1>
+                <div className="row mt-5">
+                    <div className="container">
+                        <form className='col-lg-6 m-auto'>
+                            <div className='form-group justify-content-center'>
+                                <label htmlFor="nameInput" className=''>Name:</label>
+                                <div className="col-sm-10 m-auto">
+                                    <input
+                                        type="text"
+                                        id='nameInput'
+                                        placeholder='Enter your name'
+                                        className='form-control text-center'
+                                    />
+                                </div>
+                            </div>
+                            <div className='form-group justify-content-center'>
+                                <label htmlFor="emailInput" className=''>Email:</label>
+                                <div className="col-sm-10 m-auto">
+                                    <input
+                                        type="email"
+                                        id='emailInput'
+                                        placeholder='me@awesome.com'
+                                        className='form-control text-center'
+                                    />
+                                </div>
+                            </div>
+                            <div className='form-group justify-content-center'>
+                                <label htmlFor="emailInput" className=''>Password:</label>
+                                <div className="col-sm-10 m-auto">
+                                    <input
+                                        type="email"
+                                        id='emailInput'
+                                        placeholder='Shhhhh'
+                                        className='form-control text-center'
+                                    />
+                                </div>
+                            </div>
+
+                            <button className='btn btn-outline-dark mt-4' onClick={this.props.loginBtnPressed}>Submit</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         )
     }
