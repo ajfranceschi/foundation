02564ce62331ba7d01f0c17d5ffecd5c38c4a522
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-08-28 15:35:18 -0400

    mend

--- fe-foundation/src/Containers/Register/register.jsx
+++ fe-foundation/src/Containers/Register/register.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './register.css';
 import Navbar from "../../Components/NavBar/navbar";
-// import bcrypt from 'bcryptjs';
 
 class Register extends Component {
     constructor(props) {
